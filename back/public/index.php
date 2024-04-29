3fb05c75e042e12c6320abba2b8d434ace0fc837
Julien Wangon <julien.wangon@gmail.com>
2024-04-29 19:19:25 +0100

    ADD: initialisation du routeur dans index.php

---
+++ back/public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../src/Router/Router.php';
+require_once __DIR__ . '/../src/controller/ServiceController.php';
+require_once __DIR__ . '/../src/repository/ServiceRepository.php';
+
+$router = new Router();
+$serviceRepository = new ServiceRepository();
+$serviceController = new ServiceController($serviceRepository);
+
+// Configuration des routes
+$router->addRoute('GET', '/api/services', [$serviceController, 'listServices']);
+
+// Dispatcher la requête
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$scriptName = parse_url($_SERVER['SCRIPT_NAME'], PHP_URL_PATH);
+
+// Enlève le nom du script du chemin pour obtenir le chemin de base du dossier
+$basePath = str_replace('/index.php', '', $scriptName);
+
+// Calcule le chemin de la route en enlevant le basePath du requestUri
+$routePath = substr($requestUri, strlen($basePath));
+
+if (empty($routePath)) {
+    $routePath = '/';  // Assurez-vous qu'il y a toujours une valeur par défaut
+}
+
+$router->dispatch($_SERVER['REQUEST_METHOD'], $routePath);
