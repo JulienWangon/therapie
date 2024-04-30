dccad1d5b39b8d33b31abf1f38e390494d58b06d
Julien Wangon <julien.wangon@gmail.com>
2024-04-30 08:26:00 +0100

    ADD: création du routing entre les pages dans App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,26 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/Home/Home';
+import MonParcours from './pages/MonParcours/MonParcours';
+import PsychoCorporelle from './pages/PsychoCorporelle/PsychoCorporelle';
+import TherapieSystemique from './pages/TherapieSystemique/TherapieSystemique';
+import ConstellationsFamiliales from './pages/ConstellationsFamilliales/ConstellationsFamiliales';
+
 
 function App() {
   return (
     <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="/accueil" element={<Home />} />
+                <Route path="/mon-parcours" element={<MonParcours />} />
+                <Route path="/therapie-systemique" element={<TherapieSystemique />} />
+                <Route path="/constellations-familiales" element={<ConstellationsFamiliales/>}/>
+                <Route path="/therapie-psychocorporelle" element={<PsychoCorporelle />} />
+            </Routes>
+        </BrowserRouter>
       
     </div>
   );
