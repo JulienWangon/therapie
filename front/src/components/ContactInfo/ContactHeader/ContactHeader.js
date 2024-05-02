663b594120fa9b6317c3f7270c0f7e936bbe87de
Julien Wangon <julien.wangon@gmail.com>
2024-05-02 07:56:32 +0100

    ADD: création du composant pour afficher les info de contact

---
+++ front/src/components/ContactInfo/ContactHeader/ContactHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './contactHeader.css';
+
+const ContactHeader = ({ numero, rue, codePostal, ville, phone}) => {
+  return (
+    <div className="contactHeader">
+        <span className="adresse">{numero}, {rue} {codePostal} {ville}</span>
+        <a className="phone" href={`tel:0${phone}`}>0{phone}</a>
+      
+    </div>
+  );
+};
+
+export default ContactHeader;
