98e0825597f1bdcc6b3be08b2af45de91a13fa02
Julien Wangon <julien.wangon@gmail.com>
2024-05-02 11:51:11 +0100

    UPDATE: suppression block if

--- front/src/components/ContactInfo/contactInfoService.js
+++ front/src/components/ContactInfo/contactInfoService.js
@@ -3,16 +3,9 @@ import instanceAxios from "../../_utils/axios";
 export const fetchContactInfo = async () => {
 
     try {
-      console.log("Making request to:", instanceAxios.defaults.baseURL + '/contact-info');
-
         const response = await instanceAxios.get('/contact-info');
-       
-        if (response.data.status === "success") {
-          return response.data.data;  // Accès aux données de contact
-      } 
-
+        return response.data.data;       
     } catch (error) {
-
         throw(error);
     }
 }
