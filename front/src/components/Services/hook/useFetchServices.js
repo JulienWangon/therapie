80d7a25f8dc6ea5ab1618872a0ab9e204f319b5c
Julien Wangon <julien.wangon@gmail.com>
2024-05-02 14:52:44 +0100

    ADD: création du hook pour la liste des services

---
+++ front/src/components/Services/hook/useFetchServices.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { fetchServices } from "../servicesService";
+
+const useFetchServices = () => {
+
+  const [services, setServices] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+      const fetchData = async () => {
+          setIsLoading(true);
+
+          try {
+              const response = await fetchServices(); 
+                setServices(response);          
+          } catch (error) {
+              setError(error.message)
+          } finally {
+              setIsLoading(false);
+          }  
+      };
+
+      fetchData();      
+  },[]);
+
+  return {services, isLoading, error}
+}
+
+export default useFetchServices;
