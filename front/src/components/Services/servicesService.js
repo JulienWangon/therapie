b94454d36c1327d8083448b126cea8d43617626a
Julien Wangon <julien.wangon@gmail.com>
2024-05-03 12:58:28 +0100

    ADD:  création du fichier de service pour les services

--- front/src/components/Services/servicesService.js
+++ front/src/components/Services/servicesService.js
@@ -4,7 +4,9 @@ export const fetchServices = async () => {
 
   try {
       const response = await instanceAxios.get('/services')
+      console.log(response.data.data);
       return response.data.data;
+     
   } catch(error) {
       throw(error);
   }
