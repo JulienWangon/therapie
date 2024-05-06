18e4e7b85eddd701373f55706dd4e3b65b80cf4f
Julien Wangon <julien.wangon@gmail.com>
2024-05-06 15:00:17 +0100

    UPDATE: ajout d'un bouton decouvrir

--- front/src/components/Services/ServiceCard/ServiceCard.js
+++ front/src/components/Services/ServiceCard/ServiceCard.js
@@ -6,7 +6,8 @@ const ServicesCard = ({ serviceName, pathImage}) => {
   return (
     <div className="servicesCard">
       <img className="serviceImg" src={pathImage} alt={`logo ${serviceName}`}/>
-      <h3 className="serviceName">{serviceName}</h3>      
+      <h3 className="serviceName">{serviceName}</h3> 
+      <button className="serviceBtn" type="button">Découvrir <i className="fas fa-arrow-right"></i></button>     
     </div>
   );
 };
