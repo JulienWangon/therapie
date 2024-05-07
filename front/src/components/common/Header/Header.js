bb11d5b11dfc8b0680209b0b34f7fc6ed14f8388
Julien Wangon <julien.wangon@gmail.com>
2024-05-07 08:59:38 +0100

    UPDATE: ajout de la modale et du formulaire de contact dans le header

--- front/src/components/common/Header/Header.js
+++ front/src/components/common/Header/Header.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 
 import Button from '../Button/Button';
 import ContactHeader from '../../ContactInfo/ContactHeader/ContactHeader';
+import ContactForm from '../../Contact/ContactForm/ContactForm';
+import Modal from '../Modal/Modal';
 
 import { useContactInfo } from '../../../context/ContactInfoContext';
 
 const Header = () => {
 
+  const [isModalOpen, setModalOpen] = useState(false);
+  const toggleModal = () => setModalOpen(!isModalOpen);
+
   const { contactInfo } = useContactInfo();
+  
   return (
     <header>
         <div className="topHeader">
@@ -21,8 +27,13 @@ const Header = () => {
                 text="contact"
                 className="contactBtn"
                 type="button"
-                colorStyle="whiteBtn"             
+                colorStyle="whiteBtn"
+                onClick={toggleModal}             
             />
+
+            <Modal isOpen={isModalOpen} onClose={toggleModal}>
+              <ContactForm />
+            </Modal>
 
             {contactInfo.map((info) => (
               <ContactHeader
