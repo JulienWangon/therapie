8f6f6d6e64f4a29a33a6fd5fa14cf6c12e801e58
Julien Wangon <julien.wangon@gmail.com>
2024-05-07 15:03:11 +0100

    UPDATE: ajout du composant page en construction sur toutes les pages

--- front/src/pages/PsychoCorporelle/PsychoCorporelle.js
+++ front/src/pages/PsychoCorporelle/PsychoCorporelle.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/common/Navbar/Navbar';
+import ComingSoon from '../../components/common/ComingSoon/ComingSoon';
 
 const PsychoCorporelle = () => {
   return (
     <>
         <Navbar/>
+        <ComingSoon/>
     </>
   );
 };
