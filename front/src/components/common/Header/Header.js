f8aaa8327302bb51c01d442ec1d7b3dcfc2313a4
Julien Wangon <julien.wangon@gmail.com>
2024-05-28 14:12:37 +0100

    UPDATE: modification du header

--- front/src/components/common/Header/Header.js
+++ front/src/components/common/Header/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
 
         <div className="bottomHeader">
             <Button
-                text="contact"
+                text="Contact"
                 className="contactBtn"
                 type="button"
                 colorStyle="whiteBtn"
