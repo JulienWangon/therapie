6cbe8ceb054daae63a423c114bf700c98cced040
Julien Wangon <julien.wangon@gmail.com>
2024-05-28 14:14:10 +0100

    UPDATE: modification des informations de contact du header

--- front/src/components/ContactInfo/ContactHeader/ContactHeader.js
+++ front/src/components/ContactInfo/ContactHeader/ContactHeader.js
@@ -5,7 +5,7 @@ import './contactHeader.css';
 const ContactHeader = ({ numero, rue, codePostal, ville, phone}) => {
   return (
     <div className="contactHeader">
-        <span className="adresse">{numero}, {rue} {codePostal} {ville}</span>
+        <span className="adresse">{numero} {rue} {codePostal} {ville}</span>
         <a className="phone" href={`tel:0${phone}`}>0{phone}</a>
       
     </div>
